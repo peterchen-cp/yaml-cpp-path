b972151790ee46e071a117ce8476e1c238453750
ph <sib@cherea.de>
2019-02-20 20:14:45 +0100

    Added YAPATE - play with YAML paths (Windows App, requires MFC)
    This is crude, and I am not planning to make turn this into a nice project.

---
+++ interactive/YAPATE/YAPATEDlg.h
@@ -0,0 +1,46 @@
+
+// YAPATEDlg.h: Headerdatei
+//
+
+#pragma once
+
+#include <yaml-cpp/yaml.h>
+
+
+// CYAPATEDlg-Dialogfeld
+class CYAPATEDlg : public CDialog
+{
+// Konstruktion
+public:
+	CYAPATEDlg(CWnd* pParent = nullptr);	// Standardkonstruktor
+
+// Dialogfelddaten
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_YAPATE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV-Unterstützung
+
+
+// Implementierung
+protected:
+	HICON m_hIcon;
+
+	// Generierte Funktionen für die Meldungstabellen
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+   afx_msg void OnEnChangeEdPath();
+   afx_msg void OnEnChangeEdFile();
+private:
+   void UpdateOutput();
+   YAML::Node m_yfile;
+   CString m_fileError;
+public:
+   CRichEditCtrl m_edFile;
+   CComboBox m_ddMethod;
+   afx_msg void OnCbnSelchangeDdMethod();
+};
