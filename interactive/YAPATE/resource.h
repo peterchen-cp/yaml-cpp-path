fe805bd657157712ea15281e5760b86031d4b6d6
ph <sib@cherea.de>
2019-02-21 21:48:57 +0100

    YAPATE: added a few example paths to test application

--- interactive/YAPATE/resource.h
+++ interactive/YAPATE/resource.h
@@ -5,6 +5,7 @@
 #define IDD_YAPATE_DIALOG               102
 #define IDR_MAINFRAME                   128
 #define IDC_ED_PATH                     1000
+#define IDC_DD_PATH                     1000
 #define IDC_ED_FILE                     1001
 #define IDC_ED_RESULT                   1002
 #define IDC_CB_LOAD_FILE                1003
@@ -17,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
